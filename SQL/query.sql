a53c8bb79bb7da59cf85edfc22318512b729b93d
LuisRocha800 <clonegit0@gmail.com>
2023-11-15 12:44:49 -0600

    correccion sql

--- SQL/query.sql
+++ SQL/query.sql
@@ -1,14 +1,5 @@
 jdbc:derby://localhost:1527/estacionamientoBDD;create=false
 
-CREATE TABLE nfc_movements_test (
-id_movement INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-id_tag VARCHAR(30),
-date_mov DATE,
-time_mov TIME,
-status VARCHAR(30),
-tipe_movement VARCHAR(30)
-);
-
 CREATE TABLE personal (
 id_tag VARCHAR(30) PRIMARY KEY,
 nombre VARCHAR(30),
@@ -19,12 +10,11 @@ CREATE TABLE pensionados(
 id_tag VARCHAR(30) PRIMARY KEY,
 nombre VARCHAR(30),
 apellido VARCHAR(30),
-vigencia_tarjeta DATE,
-cajon VARCHAR(30)
+vigencia_tarjeta DATE
 );
 
 CREATE TABLE tarjetas (
-id_tag VARCHAR (50) PRIMARY KEY,
+id_tag VARCHAR (30) PRIMARY KEY,
 estatus VARCHAR(20)
 );
 
@@ -36,4 +26,16 @@ time_payment TIME,
 time_service INTEGER,
 amount DECIMAL(10,2),
 status VARCHAR(30)
+);
+
+CREATE TABLE nfc_movements_test (
+id_movement INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+id_tag VARCHAR(30),
+date_mov DATE,
+time_mov TIME,
+status VARCHAR(30),
+tipe_movement VARCHAR(30),
+FOREIGN KEY (id_tag) REFERENCES personal(id_tag),
+FOREIGN KEY (id_tag) REFERENCES pensionados(id_tag),
+FOREIGN KEY (id_tag) REFERENCES tarjetas(id_tag)
 );
